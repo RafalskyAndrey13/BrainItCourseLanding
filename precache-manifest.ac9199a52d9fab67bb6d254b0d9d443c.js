d1ec1a4467265501a0b2b638d7930030a9295412
RafalskyAndrey13 <mycourses13@gmail.com>
2020-03-19 15:15:10 +0200

    Updates

---
+++ precache-manifest.ac9199a52d9fab67bb6d254b0d9d443c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5c6f6b6f70eed4c521f2fae202f90d",
+    "url": "/RafalskyAndrey13/BrainItCourseLanding/index.html"
+  },
+  {
+    "revision": "93a3aeb60de70b803e57",
+    "url": "/RafalskyAndrey13/BrainItCourseLanding/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "31594aa53e70c0f17505",
+    "url": "/RafalskyAndrey13/BrainItCourseLanding/static/js/2.cfca646d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/RafalskyAndrey13/BrainItCourseLanding/static/js/2.cfca646d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a3aeb60de70b803e57",
+    "url": "/RafalskyAndrey13/BrainItCourseLanding/static/js/main.4b68b45e.chunk.js"
+  },
+  {
+    "revision": "8ce2035fb3ae98fdf646",
+    "url": "/RafalskyAndrey13/BrainItCourseLanding/static/js/runtime-main.7209cc09.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RafalskyAndrey13/BrainItCourseLanding/static/media/logo.5d5d9eef.svg"
+  }
+]);
